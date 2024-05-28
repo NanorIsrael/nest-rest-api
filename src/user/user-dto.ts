5c09f741b78bcba6921d8802226c86db6d5b982c
nanorisrael <israel.nanor@compozelabs.com>
2024-05-28 11:57:45 +0000

    Add create user dto

---
+++ src/user/user-dto.ts
@@ -0,0 +1,20 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
+  @IsEmail()
+  readonly email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  readonly first_name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  readonly last_name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  readonly avatar: string;
+}
