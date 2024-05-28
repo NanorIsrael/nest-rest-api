acdab6ad1c1f110d6da2afeedca87b9daed3d3de
nanorisrael <israel.nanor@compozelabs.com>
2024-05-28 05:47:30 +0000

    Update user module

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,45 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Param,
+  Body,
+} from '@nestjs/common';
+import { UserService } from './user.service';
+import { Observable } from 'rxjs';
+import { AxiosResponse } from 'axios';
 
-@Controller('user')
-export class UserController {}
+@Controller('api')
+export class UserController {
+  constructor(private readonly reqresService: UserService) {}
+
+  @Post('users')
+  createUser(@Body() user: any): Observable<AxiosResponse<any>> {
+    return this.reqresService.createUser(user);
+  }
+
+  @Get('users')
+  getUsers(): Observable<AxiosResponse<any>> {
+    return this.reqresService.getUsers();
+  }
+
+  @Get('users/:id')
+  getUser(@Param('id') id: number): Observable<AxiosResponse<any>> {
+    return this.reqresService.getUser(id);
+  }
+
+  @Put('users/:id')
+  updateUser(
+    @Param('id') id: number,
+    @Body() user: any,
+  ): Observable<AxiosResponse<any>> {
+    return this.reqresService.updateUser(id, user);
+  }
+
+  @Delete('users/:id')
+  deleteUser(@Param('id') id: number): Observable<AxiosResponse<any>> {
+    return this.reqresService.deleteUser(id);
+  }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,40 @@
 import { Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { AxiosResponse } from 'axios';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private httpService: HttpService) {}
+
+  getUsers(): Observable<AxiosResponse<any>> {
+    return this.httpService
+      .get('https://reqres.in/api/users')
+      .pipe(map((response) => response.data));
+  }
+
+  getUser(id: number): Observable<AxiosResponse<any>> {
+    return this.httpService
+      .get(`https://reqres.in/api/users/${id}`)
+      .pipe(map((response) => response.data));
+  }
+
+  createUser(user: any): Observable<AxiosResponse<any>> {
+    return this.httpService
+      .post('https://reqres.in/api/users', user)
+      .pipe(map((response) => response.data));
+  }
+
+  updateUser(id: number, user: any): Observable<AxiosResponse<any>> {
+    return this.httpService
+      .put(`https://reqres.in/api/users/${id}`, user)
+      .pipe(map((response) => response.data));
+  }
+
+  deleteUser(id: number): Observable<AxiosResponse<any>> {
+    return this.httpService
+      .delete(`https://reqres.in/api/users/${id}`)
+      .pipe(map((response) => response.data));
+  }
+}
